d512c36012fd7bdcc2dac6a0b0fa4286fde7c6a9
LoranceChen <w68055010@qq.com>
2017-06-12 23:19:59 +0800

    refine

--- src/test/scala/Example.scala
+++ src/test/scala/Example.scala
@@ -9,10 +9,10 @@ import scala.concurrent.Future
   *   1. meta-calling-type   2. time     3. invoke-chain-uuid    4. stack-call   5. class-path-and-method-name   6. load: params/return-result
   * output:
   *
-  * [MetaLogFuture.Input] - Wed Apr 26 10:34:52 CST 2017 - 56a74fdd-5330-4937-a881-c58714c505a5 - 1 - NeedLogMethod$.futAdd1 - Map(x -> 1)
-    [MetaLog.Input] - Wed Apr 26 10:34:53 CST 2017 - 56a74fdd-5330-4937-a881-c58714c505a5 - 2 - NeedLogMethod$.add1 - Map(x -> 1)
-    [MetaLog.Output] - Wed Apr 26 10:34:53 CST 2017 - 56a74fdd-5330-4937-a881-c58714c505a5 - 1 - NeedLogMethod$.add1 - 2
-    [MetaLogFuture.Output] - Wed Apr 26 10:34:52 CST 2017 - 56a74fdd-5330-4937-a881-c58714c505a5 - 2 - NeedLogMethod$.futAdd1 - 2
+  * LogData(1497276559579,8096b808-a687-4766-87a2-991fae4167c7,List(Info()),NeedLogMethod$.futAdd1,1,LogParams(Map(x -> 1)))
+    LogData(1497276560814,8096b808-a687-4766-87a2-991fae4167c7,List(Info()),NeedLogMethod$.add1,2,LogParams(Map(x -> 1)))
+    LogData(1497276560814,8096b808-a687-4766-87a2-991fae4167c7,List(Info()),NeedLogMethod$.add1,1,LogResult(2))
+    LogData(1497276559579,8096b808-a687-4766-87a2-991fae4167c7,List(Info()),NeedLogMethod$.futAdd1,2,LogResult(2))
   */
 object Example extends App with MetaLogger {
   NeedLogMethod.futAdd1(1)
